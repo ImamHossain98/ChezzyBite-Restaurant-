a630070e55ab76010480e0b711e14f587ff8a095
Imam Hossain <imamhossain98@gmail.com>
2023-03-08 01:06:06 +0600

    creating Menu

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Menu from './Menu';
 
 const body = () => {
     return (
         <div>
-            
+            <Menu />
         </div>
     );
 };
---
+++ src/data/dishes.js
@@ -0,0 +1,172 @@
+const dishes = [
+    {
+        id:0,
+        name: "ginger burger",
+        image : "https://images.unsplash.com/photo-1550367363-ea12860cc124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+        
+        category : "meal",
+        label: "hot",
+        price: "199",
+        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        comments :[
+            {
+                id:0,
+                rating : 5,
+                comment: "best food in the world!",
+                author: "shanto",
+                date: "2018-10-29 12:44:22.435446"
+            },
+            {
+                id:1,
+                rating : 5,
+                comment: " meal was out standing!",
+                author: "milanto",
+                date: "2018-10-29 12:44:22.435446"
+            },
+            {
+                id:2,
+                rating : 5,
+                comment: "best food in the world!",
+                author: "pikku",
+                date: "2018-10-29 12:44:22.435446"
+            },
+            {
+                id:3,
+                rating : 5,
+                comment: "best food in the world!",
+                author: "shanta",
+                date: "2018-10-29 12:44:22.435446"
+            }
+        ]
+
+
+    },
+    {
+        id:1,
+        name: "mal khor",
+        image : "https://images.unsplash.com/photo-1550367363-ea12860cc124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+        
+        category : "meal",
+        label: "hot",
+        price: "199",
+        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        comments :[
+            {
+                id:0,
+                rating : 5,
+                comment: "best mal!",
+                author: "shanto",
+                date: "2018-10-29 12:44:22.435446"
+            },
+            {
+                id:1,
+                rating : 5,
+                comment: "veryt yummi!",
+                author: "dandi tushar",
+                date: "2018-10-29 12:44:22.435446"
+            },
+            {
+                id:2,
+                rating : 5,
+                comment: "best food in the world!",
+                author: "kop",
+                date: "2018-10-29 12:44:22.435446"
+            },
+            {
+                id:3,
+                rating : 5,
+                comment: "best food in the world!",
+                author: "soso",
+                date: "2018-10-29 12:44:22.435446"
+            }
+        ]
+
+
+    },
+    {
+        id:2,
+        name: "Pinik borobhai",
+        image : "https://images.unsplash.com/photo-1550367363-ea12860cc124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+        
+        category : "meal",
+        label: "hot",
+        price: "199",
+        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        comments :[
+            {
+                id:0,
+                rating : 5,
+                comment: "boro bhai khub e pinik!",
+                author: "imamm",
+                date: "2018-10-29 12:44:22.435446"
+            },
+            {
+                id:1,
+                rating : 5,
+                comment: "oustanding boro bhai!",
+                author: "chunupuku",
+                date: "2018-10-29 12:44:22.435446"
+            },
+            {
+                id:2,
+                rating : 5,
+                comment: "amrao jabo!",
+                author: "chora sabbir",
+                date: "2018-10-29 12:44:22.435446"
+            },
+            {
+                id:3,
+                rating : 5,
+                comment: "sb hbe dot com!",
+                author: "body rafi",
+                date: "2018-10-29 12:44:22.435446"
+            }
+        ]
+
+
+    },
+    {
+        id:3,
+        name: "ginger burger",
+        image : "https://images.unsplash.com/photo-1550367363-ea12860cc124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+        
+        category : "meal",
+        label: "hot",
+        price: "199",
+        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        comments :[
+            {
+                id:0,
+                rating : 5,
+                comment: "best food in the world!",
+                author: "shanto",
+                date: "2018-10-29 12:44:22.435446"
+            },
+            {
+                id:1,
+                rating : 5,
+                comment: "best food in the world!",
+                author: "shanto",
+                date: "2018-10-29 12:44:22.435446"
+            },
+            {
+                id:2,
+                rating : 5,
+                comment: "best food in the world!",
+                author: "shanto",
+                date: "2018-10-29 12:44:22.435446"
+            },
+            {
+                id:3,
+                rating : 5,
+                comment: "best food in the world!",
+                author: "shanto",
+                date: "2018-10-29 12:44:22.435446"
+            }
+        ]
+
+
+    }
+    ]
+
+    export default dishes;
