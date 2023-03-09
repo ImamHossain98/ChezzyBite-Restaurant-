77a79990f5892369e84fca3a00c1bad60ee07b9a
Imam Hossain <imamhossain98@gmail.com>
2023-03-09 23:37:36 +0600

    date fixed

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardBody, CardImg, CardImgOverlay, CardTitle } from 'reactstrap'
+import { Card, CardBody, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 const MenuItem = (props) => {
     console.log(props);
@@ -9,7 +9,10 @@ const MenuItem = (props) => {
                 <CardBody>
                     <CardImg width="100%" alt={props.dish.name} src={props.dish.image} style={{ opacity: "0.5" }} />
                     <CardImgOverlay>
-                        <CardTitle>{props.dish.name}</CardTitle>
+                        <CardTitle
+                         style={{ cursor: 'pointer' }} 
+                        onClick = {props.DishSelect}>
+                            {props.dish.name}</CardTitle>
                     </CardImgOverlay>
                 </CardBody>
 
--- src/data/dishes.js
+++ src/data/dishes.js
@@ -1,38 +1,38 @@
 const dishes = [
     {
-        id:0,
+        id: 0,
         name: "ginger burger",
-        image : "https://images.unsplash.com/photo-1550367363-ea12860cc124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
-        
-        category : "meal",
+        image: "https://images.unsplash.com/photo-1550367363-ea12860cc124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+
+        category: "meal",
         label: "hot",
         price: "199",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-        comments :[
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        comments: [
             {
-                id:0,
-                rating : 5,
+                id: 0,
+                rating: 5,
                 comment: "best food in the world!",
                 author: "shanto",
                 date: "2018-10-29 12:44:22.435446"
             },
             {
-                id:1,
-                rating : 5,
+                id: 1,
+                rating: 5,
                 comment: " meal was out standing!",
                 author: "milanto",
                 date: "2018-10-29 12:44:22.435446"
             },
             {
-                id:2,
-                rating : 5,
+                id: 2,
+                rating: 5,
                 comment: "best food in the world!",
                 author: "pikku",
                 date: "2018-10-29 12:44:22.435446"
             },
             {
-                id:3,
-                rating : 5,
+                id: 3,
+                rating: 5,
                 comment: "best food in the world!",
                 author: "shanta",
                 date: "2018-10-29 12:44:22.435446"
@@ -42,39 +42,39 @@ const dishes = [
 
     },
     {
-        id:1,
+        id: 1,
         name: "mal khor",
-        image : "https://images.unsplash.com/photo-1550367363-ea12860cc124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
-        
-        category : "meal",
+        image: "https://images.unsplash.com/photo-1550367363-ea12860cc124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+
+        category: "meal",
         label: "hot",
-        price: "199",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-        comments :[
+        price: "100",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        comments: [
             {
-                id:0,
-                rating : 5,
+                id: 0,
+                rating: 5,
                 comment: "best mal!",
                 author: "shanto",
                 date: "2018-10-29 12:44:22.435446"
             },
             {
-                id:1,
-                rating : 5,
+                id: 1,
+                rating: 5,
                 comment: "veryt yummi!",
                 author: "dandi tushar",
                 date: "2018-10-29 12:44:22.435446"
             },
             {
-                id:2,
-                rating : 5,
+                id: 2,
+                rating: 5,
                 comment: "best food in the world!",
                 author: "kop",
                 date: "2018-10-29 12:44:22.435446"
             },
             {
-                id:3,
-                rating : 5,
+                id: 3,
+                rating: 5,
                 comment: "best food in the world!",
                 author: "soso",
                 date: "2018-10-29 12:44:22.435446"
@@ -84,39 +84,39 @@ const dishes = [
 
     },
     {
-        id:2,
+        id: 2,
         name: "Pinik borobhai",
-        image : "https://images.unsplash.com/photo-1550367363-ea12860cc124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
-        
-        category : "meal",
+        image: "https://images.unsplash.com/photo-1550367363-ea12860cc124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+
+        category: "meal",
         label: "hot",
-        price: "199",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-        comments :[
+        price: "1922",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        comments: [
             {
-                id:0,
-                rating : 5,
+                id: 0,
+                rating: 5,
                 comment: "boro bhai khub e pinik!",
                 author: "imamm",
                 date: "2018-10-29 12:44:22.435446"
             },
             {
-                id:1,
-                rating : 5,
+                id: 1,
+                rating: 5,
                 comment: "oustanding boro bhai!",
                 author: "chunupuku",
                 date: "2018-10-29 12:44:22.435446"
             },
             {
-                id:2,
-                rating : 5,
+                id: 2,
+                rating: 5,
                 comment: "amrao jabo!",
                 author: "chora sabbir",
                 date: "2018-10-29 12:44:22.435446"
             },
             {
-                id:3,
-                rating : 5,
+                id: 3,
+                rating: 5,
                 comment: "sb hbe dot com!",
                 author: "body rafi",
                 date: "2018-10-29 12:44:22.435446"
@@ -126,39 +126,39 @@ const dishes = [
 
     },
     {
-        id:3,
+        id: 3,
         name: "ginger burger",
-        image : "https://images.unsplash.com/photo-1550367363-ea12860cc124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
-        
-        category : "meal",
+        image: "https://images.unsplash.com/photo-1550367363-ea12860cc124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+
+        category: "meal",
         label: "hot",
-        price: "199",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-        comments :[
+        price: "99",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        comments: [
             {
-                id:0,
-                rating : 5,
+                id: 0,
+                rating: 5,
                 comment: "best food in the world!",
                 author: "shanto",
                 date: "2018-10-29 12:44:22.435446"
             },
             {
-                id:1,
-                rating : 5,
+                id: 1,
+                rating: 5,
                 comment: "best food in the world!",
                 author: "shanto",
                 date: "2018-10-29 12:44:22.435446"
             },
             {
-                id:2,
-                rating : 5,
+                id: 2,
+                rating: 5,
                 comment: "best food in the world!",
                 author: "shanto",
                 date: "2018-10-29 12:44:22.435446"
             },
             {
-                id:3,
-                rating : 5,
+                id: 3,
+                rating: 5,
                 comment: "best food in the world!",
                 author: "shanto",
                 date: "2018-10-29 12:44:22.435446"
@@ -167,6 +167,6 @@ const dishes = [
 
 
     }
-    ]
+]
 
-    export default dishes;
+export default dishes;
